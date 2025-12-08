be354180313854984b2ae84121dfb9dac066a571
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-08 18:26:23 +0200

    Models Tag and Article

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="articles")
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    tags = models.ManyToManyField(Tag, related_name="articles", blank=True)
+
+    is_public = models.BooleanField(default=True, help_text="Should this article be published?")
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
